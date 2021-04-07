27e4259a346e39f046eb3afdecc39d9b5aa895b4
Naveen132895 <veeranaveen.sethuraman@faceacademy.in>
2021-04-07 14:39:43 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import UrlValidator from './components/UrlValidator/UrlValidator';
 import FilterEvenOdd from './components/FilterEvenOdd/FilterEvenOdd';
-import ImageLoader from './components/ImageLoader/ImageLoader';
-import AreaCalculator from './components/AreaCalculator/AreaCalculator';
 
 function App() {
   return (
     <div className="App">
-      {/* <AreaCalculator /> */}
       <FilterEvenOdd />
-      {/* <ImageLoader />/ */}
-      {/* <UrlValidator /> */}
     </div>
   );
 }
